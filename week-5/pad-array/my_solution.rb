3d75bdb4d3c55d0f225eac324a11ececd3bb7be1
Chris Guard <cguard90@gmail.com>
2015-10-28 13:11:46 -0400

    add solution to problem

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -19,17 +19,53 @@
 
 
 # 1. Initial Solution
-def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+def pad!(array, min_size, value = nil)
+  while array.length < min_size
+    array.push(value)
+  end
+  array
 end
+
+#If the minimum size is less than or equal to the length of the array, it should just return the array.
 
 def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+  new_array = []
+  array.each { |x|
+    new_array << x
+  }
+  array_length = new_array.length
+  if min_size <= array_length || min_size == 0  ###
+    return new_array
+  else
+    array_difference = min_size - array_length  ###
+    array_difference.times {
+      new_array << value
+    }
+    new_array
+  end
 end
 
-
 # 3. Refactored Solution
+#we were not able to successfully refactor this... after 2 hrs of working on this, we decided to call it good
+=begin
+def pad(array, min_size, value = nil) #non-destructive
+  new_array = []
+  array.each { |x|
+    new_array << x
+  }
 
+  array_length = new_array.length
 
+  if min_size <= array_length || min_size == 0  ###
+    return new_array
+  else
+    array_difference = min_size - array_length  ###
+    array_difference.times {new_array << value}
+
+    new_array
+
+  end
+end
+=end
 
 # 4. Reflection
