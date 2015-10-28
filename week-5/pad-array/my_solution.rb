4d49a64ff0928cc830fc0c640d8d1e002c9fd5de
Chris Guard <cguard90@gmail.com>
2015-10-28 13:21:01 -0400

    Add reflection to solution

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -69,3 +69,20 @@ end
 =end
 
 # 4. Reflection
+
+=begin
+    Were you successful in breaking the problem down into small steps?
+For the destructive method, yes. For the non-destructive method, not so much.
+    Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have?
+Yes for the destructive method. having to create a new object ID was an unexpected obstacle for the non-destructive method.
+
+    Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+On the destructive method, it actually went very smoothly. For the destructive method, we had to pull in a lot of resources, and ended up returning a whole new array which we created.
+
+    When you refactored, did you find any existing methods in Ruby to clean up your code?
+After 2 hours, we called it good. We were on to some potential solutions, but wanted a break. So at this point in time, no we did not successfully refactor, but I believe the .collect method, and we looked into a ternary operator as potential solutions.
+    How readable is your solution? Did you and your pair choose descriptive variable names?
+I think our solutions are very readable, and our variable names are specific and easy to follow.
+    What is the difference between destructive and non-destructive methods in your own words?
+destructive methods are easier to work with because they apply the changes that you are coding to the original array. non-destructive methods are better for preserving information becuase you are pushing the contents into a new location, and making the changes to the copy.
+=end
