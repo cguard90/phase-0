6ac59803476a336a49351cece1e5cf3cbe391dd6
Chris Guard <cguard90@gmail.com>
2015-11-04 17:30:37 -0500

    Added ending reflection

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -51,4 +51,17 @@ hello.salutation
     Is this code simpler than the last?
   yes
 
+    What is a reader method?
+A reader method allows data from within one class be accessed from outside the class using the .name method.
+    What is a writer method?
+A writer method allows data from within one class to be changed while you are outside of that class. I didn't use this one in this challenge.
+    What do the attr methods do for you?
+Allow data to flow easily between methods within a class, or possibly from outside the class altogether.
+
+    Should you always use an accessor to cover your bases? Why or why not?
+not always. You may not want the information to be available outside of that instance, or even from within that particular class. An example would be private information, you don't want it to be easily accessed from outside the class.
+
+    What is confusing to you about these methods?
+Remembering that attr_reader had to be accompanied by class.name; I thought you could just call the class, total mental blerp on my part. That's ok though, I attended office hours so that was all I needed.
+
 =end
