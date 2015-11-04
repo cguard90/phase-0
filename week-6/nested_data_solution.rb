323560b004f90bdfa2bcab96d99f95e9fc4ea408
Chris Guard <cguard90@gmail.com>
2015-11-04 17:22:10 -0500

    Add reflection to 6.5

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -78,3 +78,15 @@ startup_names.flatten!
 startup_names.map! { |word|
   word += 'ly'
 }
+
+#reflect
+=begin
+    What are some general rules you can apply to nested arrays?
+general rules... nesting can be handy but I'm not sure if it's the best way to store data. I think as a rule, don't name an array hash, or name a hash as array (keep labeling clear).
+
+    What are some ways you can iterate over nested arrays?
+squash them. that's a nice way (kind of kidding). .map, .each are options. knowing how many nested elements there are is handy, but if you're working with data that's 1k items + in length, you may not know exactly how many data points are nested. For now, squashing seems nice to me.
+
+    Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+Flatten was new to me. I had known about it but had never used it before. For refactoring, it seemed like a good time to try it out. it takes all nested arrays within an array, and essentially moves them all to the first array, and then deletes the now empty arrays. In a literal sense, it goes through and deletes all but the first and last brackets.
+=end
